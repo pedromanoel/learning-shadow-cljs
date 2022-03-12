60e56c4b727ff0c4de9b1fe9736835a32c18a180
Pedro Evangelista <pedro.evangelista@gmail.com>
2022-03-12 15:11:35 -0300

    Add karma

--- src/main/codes/pedromanoel/frontend/app.cljs
+++ src/main/codes/pedromanoel/frontend/app.cljs
@@ -1,4 +1,8 @@
 (ns codes.pedromanoel.frontend.app)
 
 (defn init []
-  (println "Hello World!"))
+  (println "Hello World!")
+  (-> js/document
+      (.getElementById "root")
+      (.-innerHTML)
+      (set! "Hello World")))
